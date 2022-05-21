6f85597f9d0543491f64e75a2911a76fc7f6714a
Autumnal_Joy <autumnal_joy@foxmail.com>
2022-05-21 19:44:41 +0800

    feat: 实现前端搜索
    前端做了搜索框和列表，点击搜索的时候，向后端api\search路径发起Get请求，参数q是搜索字符串。
    Get异步拿到数据后，用列表显示。

--- Lab2/app/src/App.js
+++ Lab2/app/src/App.js
@@ -1,23 +1,41 @@
-import logo from "./logo.svg";
+import { Input, Layout, List, Typography } from 'antd';
+import { useState } from 'react';
 import "./App.css";
 
+const { Header, Footer, Content } = Layout;
+const { Text } = Typography;
+const { Search } = Input;
+
 function App() {
+  const [listData, setListData] = useState([])
+
+  const onSearch = async (value) => {
+    const response = await fetch(`api/search?q=${value}`);
+    const data = await response.text();
+    console.log(data);
+    setListData([data]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header>Header</Header>
+        <Content>
+          <Search placeholder="input search text" onSearch={onSearch} enterButton />
+          <List
+            header={<div>标题</div>}
+            footer={<div>尾注</div>}
+            bordered
+            dataSource={listData}
+            renderItem={item => (
+              <List.Item>
+                <Text mark>[震惊！]</Text> {item}
+              </List.Item>
+            )}
+          />
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
     </div>
   );
 }
