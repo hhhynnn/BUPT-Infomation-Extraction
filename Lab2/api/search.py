575b948c12f7a760bc2eb40a06cf01f176103db8
hhhynnn <2030206080@qq.com>
2022-05-24 12:57:20 +0800

    tf-idf算法和bert算法放同一个文件,通过传递不同参数实现调用

--- Lab2/api/search.py
+++ Lab2/api/search.py
@@ -1,6 +1,7 @@
 ############################################################
-# 功能: 使用 TD-IDF 算法实现搜索
-# 参数: 搜索关键字
+# 功能: 实现搜索
+# 参数1: 搜索算法 (可选: tfidf, bert)
+# 参数2: 搜索关键字
 # 返回值: list, 其中 list 的元素类型为 dict
 #        dict 格式为 {'title':xx, # 文章标题
 #                    'time' :xx, # 发布时间
@@ -19,7 +20,8 @@ from debug_function import *
 from config import CACHE_PATH
 
 # 获取搜索关键词
-keywords_ori = sys.argv[1]
+mode = sys.argv[1]
+keywords_ori = sys.argv[2]
 keywords_ori = re.split(r'\s+', keywords_ori)
 keywords = []
 for keyword in keywords_ori:
@@ -64,62 +66,88 @@ for article in article_list:
     }
 
 print(f"{timestamp()}# 预处理完成, 用时{runtime()}")
+
+
 ##################################################
+# mode = tfidf
 # 向量空间检索算法实现
 # 1. 利用 TF-IDF 计算词向量
 # 2. 计算各文章与查询的向量内积
 # 3. 按内积表现的相关度排序
 # 4. 输出
 ##################################################
+def search_by_tfidf():
+    ##############################
+    # 1. 计算词向量
+    ##############################
+    tf_dict = defaultdict(dict)  # [文章id][词word] -> word 在 文章i 中的出现次数
+    idf_dict = dict()  # [词word] -> word 在各个文章中的出现次数
 
-##############################
-# 1. 计算词向量
-##############################
-tf_dict = defaultdict(dict)  # [文章id][词word] -> word 在 文章i 中的出现次数
-idf_dict = dict()  # [词word] -> word 在各个文章中的出现次数
-
-# 计算 tf
-for k, article in article_dict.items():
-    # 对 content 分词
-    seg_list = seg_dict[f'{k}']
-    word_set = set(seg_list)
-    # 统计 tf 和 df
+    # 计算 tf
+    for k, article in article_dict.items():
+        # 对 content 分词
+        seg_list = seg_dict[f'{k}']
+        word_set = set(seg_list)
+        # 统计 tf 和 df
+        for keyword in keywords:
+            tf_dict[k][keyword] = seg_list.count(keyword)
+    # 计算 idf
+    article_set = set(article_ids)
+    doc_cnt = len(article_ids)  # 文章个数
     for keyword in keywords:
-        tf_dict[k][keyword] = seg_list.count(keyword)
-# 计算 idf
-article_set = set(article_ids)
-doc_cnt = len(article_ids)  # 文章个数
-for keyword in keywords:
-    doc_set = set(index[keyword])
-    idf_dict[keyword] = math.log10(doc_cnt / len(doc_set & article_set))
+        doc_set = set(index[keyword])
+        idf_dict[keyword] = math.log10(doc_cnt / len(doc_set & article_set))
 
-# 计算 tf-idf 向量
-tfidf_vec_dict = defaultdict(list)
-std_vec = [idf_dict[key] for key in keywords]
-for i in article_ids:
-    tfidf_vec_dict[i] = [tf_dict[i][key] * idf_dict[key] for key in keywords]
-print(f"{timestamp()}# TF-IDF 计算完成, 用时{runtime()}")
+    # 计算 tf-idf 向量
+    tfidf_vec_dict = defaultdict(list)
+    std_vec = [idf_dict[key] for key in keywords]
+    for i in article_ids:
+        tfidf_vec_dict[i] = [tf_dict[i][key] * idf_dict[key] for key in keywords]
+    print(f"{timestamp()}# TF-IDF 计算完成, 用时{runtime()}")
 
-##############################
-# 2. 计算向量内积
-##############################
-sc_dict = dict()  # 相似度字典
-vec_len = len(std_vec)  # 向量长度
-for i in article_ids:
-    sc_dict[i] = 0
-    i_vec = tfidf_vec_dict[i]  # 向量 i
-    for idx in range(vec_len):
-        sc_dict[i] += std_vec[idx] * i_vec[idx]
+    ##############################
+    # 2. 计算向量内积
+    ##############################
+    sc_dict = dict()  # 相似度字典
+    vec_len = len(std_vec)  # 向量长度
+    for i in article_ids:
+        sc_dict[i] = 0
+        i_vec = tfidf_vec_dict[i]  # 向量 i
+        for idx in range(vec_len):
+            sc_dict[i] += std_vec[idx] * i_vec[idx]
 
-print(f"{timestamp()}# 向量内积计算完成, 用时{runtime()}")
+    print(f"{timestamp()}# 向量内积计算完成, 用时{runtime()}")
 
-##############################
-# 3. 结果排序并输出
-##############################
-seq = sorted(sc_dict.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+    ##############################
+    # 3. 结果排序并输出
+    ##############################
+    seq = sorted(sc_dict.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+    print(f"{timestamp()}# 排序计算完成, 用时{runtime()}")
+    return seq
 
+
+##################################################
+# mode = bert
+# bert 检索算法实现
+##################################################
+def search_by_bert():
+    return []
+    pass
+
+
+if mode == 'tfidf':
+    result_id_list = search_by_tfidf()
+elif mode == 'bert':
+    result_id_list = search_by_bert()
+else:
+    result_id_list = []
+    print('param1 must be "tfidf" or "bert"')
+
+##################################################
+# 根据排序返回要显示的内容
+##################################################
 result = []
-for i, sc in seq:
+for i, sc in result_id_list:
     d = {'title': article_dict[i]['title'],
          'time': article_dict[i]['time'],
          'url': article_dict[i]['url']
@@ -137,8 +165,3 @@ for i, sc in seq:
         d['relate'] = relate
         break
     result.append(d)
-
-print(f"{timestamp()}# 排序计算完成, 用时{runtime()}")
-
-for each in result[:5]:
-    print(each)
